0574c258471810cf26766081940635502431e577
root <shwetadesale52@gmail.com>
2024-09-30 00:26:53 +0100

    chnages in code to check cicd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Good day!')
+  res.send('Hello World! Good night :) !!!!!!!')
 })
 
 app.get('/status', (req, res) => {
